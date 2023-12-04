aabdf736ea9c0acc40fca5c02900887ae4d6127d
Maks <mhromyk.edu@gmail.com>
2023-12-04 20:03:13 +0100

    initial commit: empty project

---
+++ src/main/java/mh/springboot/restapiplayground/RestApiPlaygroundApplication.java
@@ -0,0 +1,13 @@
+package mh.springboot.restapiplayground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiPlaygroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiPlaygroundApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mh/springboot/restapiplayground/RestApiPlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package mh.springboot.restapiplayground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiPlaygroundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
