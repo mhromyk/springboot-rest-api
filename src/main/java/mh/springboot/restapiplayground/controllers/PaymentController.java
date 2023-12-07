9bae5faa56cef0f55792f80a0634a2ad7cb014e0
Maks <mhromyk.edu@gmail.com>
2023-12-07 16:57:17 +0100

    REST-API with exception handler via controller advice

--- src/main/java/mh/springboot/restapiplayground/controllers/PaymentController.java
+++ src/main/java/mh/springboot/restapiplayground/controllers/PaymentController.java
@@ -1,15 +1,12 @@
 package mh.springboot.restapiplayground.controllers;
 
-import mh.springboot.restapiplayground.model.ErrorDetails;
 import mh.springboot.restapiplayground.model.PaymentDetails;
-import mh.springboot.restapiplayground.model.exceptions.NotEnoughMoneyException;
 import mh.springboot.restapiplayground.services.PaymentService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import static org.springframework.http.HttpStatus.ACCEPTED;
-import static org.springframework.http.HttpStatus.BAD_REQUEST;
 
 @RestController
 public class PaymentController {
@@ -22,12 +19,7 @@ public class PaymentController {
 
     @PostMapping("/payment")
     public ResponseEntity<?> makePayment() {
-        try {
-            PaymentDetails paymentDetails = paymentService.proceed();
-            return ResponseEntity.status(ACCEPTED).body(paymentDetails);
-        } catch (NotEnoughMoneyException exception) {
-            return ResponseEntity.status(BAD_REQUEST).body(
-                    new ErrorDetails("Not enough money on the account. Declined by payment system."));
-        }
+        PaymentDetails paymentDetails = paymentService.proceed();
+        return ResponseEntity.status(ACCEPTED).body(paymentDetails);
     }
 }
---
+++ src/main/java/mh/springboot/restapiplayground/controllers/exceptions/ExceptionControllerAdvice.java
@@ -0,0 +1,19 @@
+package mh.springboot.restapiplayground.controllers.exceptions;
+
+import mh.springboot.restapiplayground.model.ErrorDetails;
+import mh.springboot.restapiplayground.model.exceptions.NotEnoughMoneyException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+
+@RestControllerAdvice
+public class ExceptionControllerAdvice {
+
+
+    @ExceptionHandler(value = NotEnoughMoneyException.class)
+    public ResponseEntity<ErrorDetails> notEnoughMoney() {
+        return ResponseEntity.status(BAD_REQUEST).body(new ErrorDetails("Not enough money on the account."));
+    }
+}
